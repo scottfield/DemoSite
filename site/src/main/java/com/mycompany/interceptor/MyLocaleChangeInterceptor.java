dc7acded43cab4d57e1d9ba1d0e0278e98033e81
jackie <scottfield@qq.com>
2016-03-24 17:56:38 +0800

    add chinese international

---
+++ site/src/main/java/com/mycompany/interceptor/MyLocaleChangeInterceptor.java
@@ -0,0 +1,10 @@
+package com.mycompany.interceptor;
+
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Created by jackie on 2016/3/24.
+ */
+public class MyLocaleChangeInterceptor extends LocaleChangeInterceptor {
+
+}
